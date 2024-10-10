8ffa92417a0fe56ee6793355fd3a6d812eedb173
Dennis Amematekpor <dennisamematekpor>
2024-10-10 15:54:48 +0200

    dunders in python

--- objected_oriented_programming.py
+++ objected_oriented_programming.py
@@ -36,6 +36,22 @@ class Employee:
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True 
+    
+    def __add__(self):
+        return f"{self.pay}"
+
+    def __repr__(self):
+        return Employee(f"{self.first} {self.last} {self.pay}")
+
+    def __str__(self):
+        return f"{self.fullname()} - {self.email}"
+    
+    def __add__(self, other):
+        return self.pay + other.pay
+    
+    def __len__(self):
+        return len(self.fullname())
+
 
 class Developer(Employee):
     raise_amount = 1.10
@@ -43,6 +59,9 @@ class Developer(Employee):
     def __init__(self, first, last, pay, prog_lang):
         super().__init__(first, last, pay)
         self.prog_lang = prog_lang
+
+
+
 
 class Manager(Employee):
     def __init__(self, first, last, pay, employees = None):
@@ -62,7 +81,9 @@ class Manager(Employee):
 
     def all_emps(self):
         for emp in self.employees:
-            print("--->", emp.fullname())          
+            print("--->", emp.fullname())     
+
+
 
 
 dev_1 = Employee("Dennis","Amematekpor", 602340)
@@ -78,12 +99,17 @@ my_date = datetime.date(2000, 1, 22)
 
 mgr_1.add_emp(emp2)
 
-print(mgr_1.email)
-mgr_1.all_emps()
-print(mgr_1.employees[0].fullname())
+#print(mgr_1.email)
+#mgr_1.all_emps()
+#print(mgr_1.employees[0].fullname())
 
 mgr_1.remove_emp(dev_1)
-mgr_1.all_emps()
+#mgr_1.all_emps()
 
-print(isinstance(mgr_1, Employee))
-print(issubclass(Manager, Developer))
+#print(isinstance(mgr_1, Employee))
+#print(issubclass(Manager, Developer))
+
+print(dev_1)
+print(str(dev_1))
+print(dev_1 + mgr_1)
+print(len(emp2))
